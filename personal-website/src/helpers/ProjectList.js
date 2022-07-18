ae5b713024ce51f728ad8f4704b59cab6376a958
hevanli <67269156+hevanli@users.noreply.github.com>
2022-07-18 15:37:05 -0700

    Finished Tutorial

---
+++ personal-website/src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/proj1.jpeg";
+import Proj2 from "../assets/proj2.jpeg";
+import Proj3 from "../assets/proj3.webp";
+import Proj4 from "../assets/proj4.webp";
+import Proj5 from "../assets/proj5.jpeg";
+import Proj6 from "../assets/proj6.png";
+
+export const ProjectList = [
+    {
+        name: "Dijkstra Algorithm Visualizer",
+        image: Proj1,
+        skills: "JS, HTML, CSS",
+    },
+    {
+        name: "Ecommerce Website",
+        image: Proj2,
+        skills: "React, Node JS, MongoDB",
+    },
+    {
+        name: "Spotify Clone",
+        image: Proj3,
+        skills: "Yuh placeholder, a;skldjfasdl;kfj",
+    },
+    {
+        name: "Social Media Website",
+        image: Proj4,
+        skills: "C++ idk this is, just, stuff",
+    },
+    {
+        name: "Dashboard Visualizer",
+        image: Proj5,
+        skills: "skill1, skill2, skill3",
+    },
+    {
+        name: "Mobile Game",
+        image: Proj6,
+        skills: "skill 4, skill 6, skill 345",
+    }
+]
